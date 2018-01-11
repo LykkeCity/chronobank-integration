f5586a7ec67250567624f779bd6c859a674d7379
Troshkin Sergey <legat.psu@gmail.com>
2018-01-11 20:02:12 +0500

    add gas price estimating

--- src/LkeServices/Signature/LykkeSignedTransactionManager.cs
+++ src/LkeServices/Signature/LykkeSignedTransactionManager.cs
@@ -6,6 +6,7 @@ using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
 using Core.Providers;
+using Core.Settings;
 using Nethereum.Hex.HexTypes;
 using Nethereum.JsonRpc.Client;
 using Nethereum.RPC.Eth.DTOs;
@@ -16,32 +17,36 @@ using Nethereum.RPC.NonceServices;
 using Nethereum.RPC.TransactionManagers;
 using Nethereum.RPC.TransactionReceipts;
 using Nethereum.Signer;
+using Nethereum.Util;
 using Transaction = Nethereum.Signer.Transaction;
 
 namespace LkeServices.Signature
 {
     public class LykkeSignedTransactionManager : ITransactionManager
     {
-        private BigInteger DefaultGasPriceConst = BigInteger.Parse("20000000000");
-
-        private BigInteger _nonceCount = -1;
+        private readonly Web3 _web3;
         private readonly ISignatureApi _signatureApi;
 
         private readonly ConcurrentDictionary<string, INonceService> _nonceServices = new ConcurrentDictionary<string, INonceService>();
+        private readonly BigInteger _minGasPrice;
+        private readonly BigInteger _maxGasPrice;
 
-        public LykkeSignedTransactionManager(Web3 web3, ISignatureApi signatureApi)
+        public LykkeSignedTransactionManager(Web3 web3, ISignatureApi signatureApi, BaseSettings baseSettings)
         {
+            _web3 = web3;
             _signatureApi = signatureApi;
             Client = web3.Client;
+            _minGasPrice = UnitConversion.Convert.ToWei(baseSettings.MinGasPriceGwei, UnitConversion.EthUnit.Gwei);
+            _maxGasPrice = UnitConversion.Convert.ToWei(baseSettings.MaxGasPriceGwei, UnitConversion.EthUnit.Gwei);
         }
 
         private async Task<HexBigInteger> GetNonceAsync(TransactionInput transaction)
-        {                                  
+        {
             var nonce = transaction.Nonce;
             if (nonce == null)
             {
                 var nonceService = _nonceServices.GetOrAdd(transaction.From, (key) => new InMemoryNonceService(key, Client));
-                nonce = await nonceService.GetNextNonceAsync();               
+                nonce = await nonceService.GetNextNonceAsync();
             }
             return nonce;
         }
@@ -52,7 +57,7 @@ namespace LkeServices.Signature
 
             var nonce = await GetNonceAsync(transaction);
             var value = transaction.Value?.Value ?? 0;
-            var gasPrice = transaction.GasPrice?.Value ?? DefaultGasPriceConst;
+            var gasPrice = await EstimateGasPrice();
             var gasValue = transaction.Gas?.Value ?? Transaction.DEFAULT_GAS_LIMIT;
 
             var tr = new Transaction(transaction.To, value, nonce, gasPrice, gasValue, transaction.Data);
@@ -61,6 +66,13 @@ namespace LkeServices.Signature
 
             return await ethSendTransaction.SendRequestAsync(response.SignedTransaction.EnsureHexPrefix()).ConfigureAwait(false);
         }
+
+        public async Task<BigInteger> EstimateGasPrice()
+        {
+            var currentGasPrice = (await _web3.Eth.GasPrice.SendRequestAsync()).Value;
+            return BigInteger.Max(BigInteger.Min(currentGasPrice, _maxGasPrice), _minGasPrice);
+        }
+
 
         public Task<HexBigInteger> EstimateGasAsync<T>(T callInput) where T : CallInput
         {
